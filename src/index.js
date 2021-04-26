9ca97cf6addfc7f73a2cf07298b479f149b23fc6
PeHaa <info@pehaa.com>
2021-04-26 06:41:25 +0200

    google fonts

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import "bootstrap/dist/css/bootstrap.css"
-import "@fontsource/kiwi-maru"
 import "./index.css"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
